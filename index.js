3a7ea07d3cebbc8781faa4d14bf8b788d6573149
GURU <lylasofia462@gmail.com>
2025-10-16 04:12:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 import express from "express";
-import bodyParser from "body-parser";
 import pairRouter from "./pair.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public")); // for HTML/CSS/JS files
 
-// Serve main page
+// Root route
 app.get("/", (req, res) => {
-  res.send("<h1>💫 LUNA Pairing Server is alive!</h1>");
+  res.sendFile(new URL("./public/main.html", import.meta.url));
 });
 
 // Use pairing router
 app.use("/code", pairRouter);
 
-app.listen(PORT, () => {
-  console.log(`🚀 LUNA Pair Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`🚀 LUNA Pair Server running on port ${PORT}`));
